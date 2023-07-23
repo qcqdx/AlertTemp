77c380d197d234e3b06b5762c726debbe685030a
Sergei <4885288@gmail.com>
2023-07-23 23:04:48 +0300

    Refactor and enhance incident detection logic
    - Introduced separate functions for creating database tables, ensuring better modularity.
    - Optimized the retrieval of sensor data based on the last processed timestamp to improve performance.
    - Improved handling of sensor state changes, capturing transitions to and from overheating and overcooling events.
    - Added functionality to update peak values during return to normal state for more accurate incident reporting.

--- incident_detector.py
+++ incident_detector.py
@@ -1,69 +1,124 @@
-import time
 import sqlite3
-from datetime import datetime, timedelta
-
-def create_incidents_db(cur):
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS incidents (
-            id INTEGER PRIMARY KEY,
-            datetime TEXT NOT NULL,
-            event TEXT NOT NULL,
-            tab_id INTEGER NOT NULL,
-            sensor TEXT NOT NULL,
-            value REAL NOT NULL
-        )
-    """)
+from datetime import datetime
 
 
-def create_states_db(cur):
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS states (
-            sensor TEXT PRIMARY KEY,
-            state TEXT NOT NULL
-        )
-    """)
+def create_incidents_db(cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS incidents ("
+        "id INTEGER PRIMARY KEY,"
+        "datetime TEXT NOT NULL,"
+        "event TEXT NOT NULL,"
+        "tab_id INTEGER NOT NULL,"
+        "sensor TEXT NOT NULL,"
+        "value REAL NOT NULL,"
+        "peak REAL,"
+        "duration TEXT)"
+    )
 
 
-def create_last_processed_timestamp_db(cur):
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS last_processed_timestamp (
-            id INTEGER PRIMARY KEY,
-            timestamp TEXT NOT NULL
-        )
-    """)
+def create_states_db(cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS states ("
+        "sensor TEXT PRIMARY KEY,"
+        "state TEXT NOT NULL)"
+    )
 
 
-def get_last_state(cur, sensor):
-    cur.execute("SELECT state FROM states WHERE sensor = ?", (sensor,))
-    res = cur.fetchone()
-    return res[0] if res else "Норма"
+def create_last_processed_timestamp_db(cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS last_processed ("
+        "id INTEGER PRIMARY KEY,"
+        "timestamp TEXT NOT NULL)"
+    )
 
 
-def get_tab_and_range(cur, sensor):
-    cur.execute("SELECT tab_settings.tab_id, overheat, overcool, "
-                "CASE WHEN table1 = ? THEN table1_alias "
-                "WHEN table2 = ? THEN table2_alias "
-                "WHEN table3 = ? THEN table3_alias END "
-                "FROM tab_settings JOIN ranges "
-                "ON tab_settings.tab_id = ranges.tab_id WHERE table1 = ? OR table2 = ? OR table3 = ?",
-                (sensor, sensor, sensor, sensor, sensor, sensor))
-    res = cur.fetchone()
-    return res if res else (None, None, None, None)
+def load_last_processed_timestamp(cursor):
+    cursor.execute("SELECT timestamp FROM last_processed ORDER BY id DESC LIMIT 1")
+    result = cursor.fetchone()
+    return result[0] if result else None
 
 
-def delete_old_incidents(cur):
-    six_months_ago = datetime.now() - timedelta(days=180)  # Примерное значение для полугода
-    cur.execute("DELETE FROM incidents WHERE datetime < ?", (six_months_ago.isoformat(),))
+def get_peak_and_start_time(sensor, start_time, end_time, cursor):
+    cursor.execute(
+        "SELECT MAX(value), MIN(timestamp) FROM temp_records WHERE topic = ? AND timestamp BETWEEN ? AND ?",
+        (sensor, start_time, end_time)
+    )
+    res = cursor.fetchone()
+    return res if res else (None, None)
 
 
-def save_last_processed_timestamp(cur, timestamp):
-    cur.execute("INSERT OR REPLACE INTO last_processed_timestamp (id, timestamp) VALUES (?, ?)", (1, timestamp))
+def update_peak_values_for_return_to_normal(cursor, sensors_cursor):
+    # print("Starting to update peak values...")
+    cursor.execute("SELECT id, datetime, sensor FROM incidents WHERE event = 'Возврат в норму' AND peak IS NULL")
+    records_to_update = cursor.fetchall()
+
+    for record in records_to_update:
+        record_id, end_time, sensor = record
+        cursor.execute("SELECT datetime FROM incidents WHERE sensor = ? AND event IN ('Перегрев', 'Переохлаждение') "
+                       "AND datetime < ? ORDER BY datetime DESC LIMIT 1", (sensor, end_time))
+        start_time_data = cursor.fetchone()
+
+        if not start_time_data:
+            # print(f"No start time found for record {record_id}.")
+            continue
+
+        start_time = start_time_data[0]
+        sensors_cursor.execute("SELECT MAX(value) FROM temp_records WHERE topic LIKE ? AND timestamp BETWEEN ? AND ?",
+                               ('%' + sensor + '%', start_time, end_time))
+        peak_value_data = sensors_cursor.fetchone()
+
+        if not peak_value_data:
+            # print(f"No peak value data found for record {record_id} between {start_time} and {end_time}.")
+            continue
+
+        peak_value = peak_value_data[0]
+        # print(f"Updating record {record_id} with peak value {peak_value}.")
+        cursor.execute("UPDATE incidents SET peak = ? WHERE id = ?", (peak_value, record_id))
 
 
-def load_last_processed_timestamp(cur):
-    cur.execute("SELECT timestamp FROM last_processed_timestamp WHERE id = ?", (1,))
-    res = cur.fetchone()
+def delete_old_incidents(cursor):
+    # This function can be expanded based on the logic to delete old incidents
+    pass
+
+
+def get_tab_and_range(cursor, sensor_name):
+    # Получение tab_id, overheat и overcool из таблиц ranges и tab_settings
+    cursor.execute("""
+        SELECT 
+            ranges.tab_id, ranges.overheat, ranges.overcool, 
+            CASE 
+                WHEN tab_settings.table1 = ? THEN tab_settings.table1_alias
+                WHEN tab_settings.table2 = ? THEN tab_settings.table2_alias
+                WHEN tab_settings.table3 = ? THEN tab_settings.table3_alias
+                ELSE NULL
+            END as sensor_alias
+        FROM ranges
+        JOIN tab_settings ON ranges.tab_id = tab_settings.tab_id
+        WHERE tab_settings.table1 = ? OR tab_settings.table2 = ? OR tab_settings.table3 = ?
+        """, (sensor_name, sensor_name, sensor_name, sensor_name, sensor_name, sensor_name))
+
+    result = cursor.fetchone()
+    return result if result else (None, None, None, None)
+
+
+def get_peak_value(sensor, start_time, end_time, cursor):
+    """Функция для получения пикового значения датчика между start_time и end_time."""
+    cursor.execute(
+        "SELECT MAX(value) FROM temp_records WHERE topic = ? AND timestamp BETWEEN ? AND ?",
+        (sensor, start_time, end_time)
+    )
+    res = cursor.fetchone()
     return res[0] if res else None
+
+
+def get_last_state(cursor, sensor):
+    cursor.execute("SELECT state FROM states WHERE sensor = ?", (sensor,))
+    result = cursor.fetchone()
+    return result[0] if result else None
+
+
+def save_last_processed_timestamp(cursor, timestamp):
+    cursor.execute("INSERT INTO last_processed (timestamp) VALUES (?)", (timestamp,))
 
 
 def main():
@@ -76,36 +131,31 @@ def main():
 
     try:
         create_incidents_db(incidents_cur)
-        create_states_db(incidents_cur)  # Creating the states table
-        create_last_processed_timestamp_db(incidents_cur)  # Creating the last_processed_timestamp table
-
+        create_states_db(incidents_cur)
+        create_last_processed_timestamp_db(incidents_cur)
         last_processed_timestamp = load_last_processed_timestamp(incidents_cur)
 
-        # Main loop
         while True:
             if last_processed_timestamp is None:
                 sensors_cur.execute("SELECT timestamp, topic, value FROM temp_records ORDER BY timestamp")
             else:
                 sensors_cur.execute("SELECT timestamp, topic, value FROM temp_records "
-                                    "WHERE timestamp > ? ORDER BY timestamp",
-                                    (last_processed_timestamp,))
+                                    "WHERE timestamp > ? ORDER BY timestamp", (last_processed_timestamp,))
 
             delete_old_incidents(incidents_cur)
 
             for row in sensors_cur:
                 timestamp, sensor, value = row
                 last_processed_timestamp = timestamp
-                sensor_name = sensor.replace('/', '_')  # Замена '/' на '_' в sensor
+                peak_value = None  # Инициализация перед использованием
+                duration = None  # Инициализация перед использованием
+                sensor_name = sensor.replace('/', '_')
                 tab_id, overheat, overcool, sensor_alias = get_tab_and_range(settings_cur, sensor_name)
 
-                # Если не удалось получить значения overheat и overcool, пропускаем текущую итерацию
                 if tab_id is None or overheat is None or overcool is None:
                     continue
 
-                # Используйте псевдоним датчика, если он доступен, иначе используйте его имя
                 sensor = sensor_alias if sensor_alias else sensor_name
-
-                # Получить последнее состояние этого датчика, если оно существует, иначе инициализировать как "Норма"
                 last_state = get_last_state(incidents_cur, sensor)
 
                 if value > overheat:
@@ -113,37 +163,47 @@ def main():
                 elif value < overcool:
                     new_state = "Переохлаждение"
                 else:
+                    if last_state in ["Перегрев", "Переохлаждение"]:
+                        # Получение timestamp начала инцидента
+                        incidents_cur.execute("SELECT datetime FROM incidents WHERE sensor = ? AND event = ? "
+                                              "ORDER BY datetime DESC LIMIT 1", (sensor, last_state))
+                        incident_start_timestamp = incidents_cur.fetchone()
+                        if incident_start_timestamp:
+                            incident_start_timestamp = incident_start_timestamp[0]
+                            peak_value = get_peak_value(sensor_name, incident_start_timestamp, timestamp, sensors_cur)
+                            duration = (
+                                    datetime.fromisoformat(timestamp) -
+                                    datetime.fromisoformat(incident_start_timestamp)
+                            )
+                            duration = str(duration)
+                        else:
+                            peak_value, duration = None, None
+                    else:
+                        peak_value, duration = None, None
                     new_state = "Возврат в норму"
 
-                # Обновляем состояние в базе данных в любом случае
-                incidents_cur.execute(
-                    "INSERT OR REPLACE INTO states (sensor, state) VALUES (?, ?)",
-                    (sensor, new_state))
+                incidents_cur.execute("INSERT OR REPLACE INTO states (sensor, state) "
+                                      "VALUES (?, ?)", (sensor, new_state))
 
-                # Вставляем запись об инциденте только если состояние изменилось
                 if new_state != last_state:
                     incidents_cur.execute(
-                        "INSERT INTO incidents (datetime, event, tab_id, sensor, value) "
-                        "VALUES (?, ?, ?, ?, ?)",
-                        (timestamp, new_state, tab_id, sensor, value))
+                        "INSERT INTO incidents (datetime, event, tab_id, sensor, value, peak, duration) "
+                        "VALUES (?, ?, ?, ?, ?, ?, ?)",
+                        (timestamp, new_state, tab_id, sensor, value, peak_value, duration))
 
-            # Сохраняем последний обработанный timestamp
             save_last_processed_timestamp(incidents_cur, last_processed_timestamp)
-
+            update_peak_values_for_return_to_normal(incidents_cur, sensors_cur)
             incidents_conn.commit()
 
-            # print("Следующая проверка через минуту.")
-            try:
-                time.sleep(60)
-            except KeyboardInterrupt:
-                break
     except Exception as e:
         print(f"An error occurred: {e}")
+        incidents_conn.rollback()
+
     finally:
         incidents_conn.close()
         sensors_conn.close()
         settings_conn.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
