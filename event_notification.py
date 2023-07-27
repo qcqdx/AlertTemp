9b61c993edb3afb0439d439fc8a7f7a5dbd97a25
Sergei <4885288@gmail.com>
2023-07-27 23:10:43 +0300

    Enhanced logging capabilities for incident handling
    - Established logging for database connections and bot initializations.
    - Added detailed logging for each message sent to Telegram, including the message type and recipient.
    - Implemented logging for downtime calculations, especially when downtime exceeds one day.
    - Integrated logging for new incident detection in the database to ensure every new entry triggers a notification.

--- event_notification.py
+++ event_notification.py
@@ -3,6 +3,11 @@ import asyncio
 from datetime import datetime, timedelta
 from aiogram import Bot
 import time
+import logging
+
+# Setup logging
+logging.basicConfig(filename='instance/evnot_log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Connect to the SQLite database
 conn_incidents = sqlite3.connect('instance/incidents.db')
@@ -12,7 +17,9 @@ cur_incidents = conn_incidents.cursor()
 conn_settings = sqlite3.connect('instance/user_settings.db')
 cur_settings = conn_settings.cursor()
 
-# Проверка на наличие таблицы bots и её создание при отсутствии
+logging.info('Connected to databases.')
+
+# Check for the existence of the 'bots' table and create it if it doesn't exist
 cur_settings.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='bots'")
 if not cur_settings.fetchone():
     cur_settings.execute("""
@@ -23,17 +30,21 @@ if not cur_settings.fetchone():
     )
     """)
     conn_settings.commit()
+    logging.info("Created 'bots' table.")
 
 # Fetch all bot API keys from the database
 cur_settings.execute("SELECT api_key FROM bots")
 bot_api_keys = cur_settings.fetchall()
+logging.info(f"Fetched {len(bot_api_keys)} bot API keys.")
 
 # Fetch all user chat_ids from the database
 cur_settings.execute("SELECT userid FROM users")
 chat_ids = cur_settings.fetchall()
+logging.info(f"Fetched {len(chat_ids)} chat IDs.")
 
 # Telegram bot setup
 bots = [Bot(api_key[0]) for api_key in bot_api_keys]
+logging.info('Initialized Telegram bots.')
 
 # Create a new event loop
 loop = asyncio.new_event_loop()
@@ -41,8 +52,7 @@ loop = asyncio.new_event_loop()
 # Set the new event loop as the current event loop
 asyncio.set_event_loop(loop)
 
-previous_messages = {}  # словарь для хранения ID предыдущих сообщений
-
+previous_messages = {}
 sent_incident_ids = set()
 
 
@@ -57,11 +67,10 @@ def get_previous_incident_type(current_id, tab_id, sensor_name):
 
         event, prev_tab_id, prev_sensor = row
 
-        # Проверяем соответствие tab_id и sensor_name
         if prev_tab_id == tab_id and prev_sensor == sensor_name and event in ['Перегрев', 'Переохлаждение']:
             return event
 
-    return None  # Если не найдено соответствующего инцидента
+    return None
 
 
 def convert_tab_id(tab_id):
@@ -90,6 +99,7 @@ def format_message(incident):
             days_str, time_str = incident[7].split(' day, ')
             days = int(days_str)
             hours, minutes, rest = time_str.split(':')
+            logging.info(f"Downtime exceeded a day. Total days: {days}.")
         else:
             days = 0
             hours, minutes, rest = incident[7].split(':')
@@ -101,10 +111,12 @@ def format_message(incident):
             total_seconds += 1
 
         downtime = str(timedelta(seconds=total_seconds))
+        logging.info(f"Total downtime in seconds: {total_seconds}.")
 
         message = (f"<b>{tab_name}, {incident[4]},\n{emoji} {incident[2]},</b> со значением: <b>{incident[5]} ℃.</b>"
                    f"\nВ состоянии <b>{prev_incident_type}</b> находился <b>{downtime}</b>"
                    f"\n<i>Зафиксировано:</i> \n<b>{datetime_corrected_str}</b>\n")
+
 
     else:
         message = (f"<b>{tab_name}, {incident[4]},\n{emoji} {incident[2]},</b> со значением: <b>{incident[5]} ℃.</b>"
@@ -114,60 +126,104 @@ def format_message(incident):
 
 
 async def send_incident(incident):
-    incident_id = incident[0]  # ID инцидента
+    incident_id = incident[0]
 
-    # Проверяем, был ли этот инцидент уже отправлен
     if incident_id in sent_incident_ids:
         return
 
     message_to_send = format_message(incident)
+
     key = (convert_tab_id(incident[3]), incident[4])
 
     for bot in bots:
+
         for chat_id in chat_ids:
-            try:  # добавляем блок try
+
+            try:
+
                 if incident[2] == 'Возврат в норму' and key in previous_messages:
+
                     reply_to_id = previous_messages[key].pop()
-                    await bot.send_message(chat_id[0], message_to_send, parse_mode='HTML', reply_to_message_id=reply_to_id)
+
+                    await bot.send_message(chat_id[0], message_to_send, parse_mode='HTML',
+                                           reply_to_message_id=reply_to_id)
+
+                    logging.info(
+                        f"Sent 'Возврат в норму' message to chat_id {chat_id[0]} using bot {bot._token.split(':')[0]} with reply_to_id {reply_to_id}. Message: {message_to_send}")
+
                     if not previous_messages[key]:
                         del previous_messages[key]
+
                 else:
+
                     sent_message = await bot.send_message(chat_id[0], message_to_send, parse_mode='HTML')
+
+                    logging.info(
+                        f"Sent '{incident[2]}' message to chat_id {chat_id[0]} using bot {bot._token.split(':')[0]}. Message: {message_to_send}")
+
                     if incident[2] in ['Перегрев', 'Переохлаждение']:
+
                         if key not in previous_messages:
                             previous_messages[key] = []
-                        previous_messages[key].append(sent_message.message_id)
-            except Exception as e:  # обрабатываем исключение
-                print(f"Error sending message to chat_id {chat_id[0]} using bot {bot._token.split(':')[0]}: {e}")
 
-                continue
+                        previous_messages[key].append(sent_message.message_id)
+
+            except Exception as e:
+
+                logging.error(
+                    f"Error sending message to chat_id {chat_id[0]} using bot {bot._token.split(':')[0]}: {e}")
+
     sent_incident_ids.add(incident_id)
 
+
 # Get the id of the latest incident
+
 cur_incidents.execute("SELECT * FROM incidents ORDER BY id DESC LIMIT 1")
+
 last_incident = cur_incidents.fetchone()
 
 if last_incident is not None:
+
     loop.run_until_complete(send_incident(last_incident))
+
     last_id = last_incident[0]
+
 else:
+
     last_id = 0
 
 try:
+
     while True:
+
         cur_incidents.execute("SELECT * FROM incidents WHERE id > ?", (last_id,))
+
         new_incidents = cur_incidents.fetchall()
 
         if new_incidents:
+            logging.info(f"Found {len(new_incidents)} new incidents in the database.")
             for incident in new_incidents:
                 loop.run_until_complete(send_incident(incident))
                 last_id = incident[0]
+
         time.sleep(10)
+
 except KeyboardInterrupt:
+
+    logging.info("KeyboardInterrupt received. Exiting...")
+
     pass
+
 finally:
+
     for bot in bots:
         loop.run_until_complete(bot.session.close())
 
+        logging.info(f"Closed session for bot {bot._token.split(':')[0]}.")
+
 for bot in bots:
     loop.run_until_complete(bot.close())
+
+    logging.info(f"Closed bot {bot._token.split(':')[0]}.")
+
+logging.info("Program finished.")
